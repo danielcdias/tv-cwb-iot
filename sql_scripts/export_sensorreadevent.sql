87551151da3b396f9688e277fa0d82aec596e90b
Daniel Carvalho Dias <daniel.dias@gmail.com>
2020-01-29 21:43:07 -0300

    Melhoria na query SQL de exporte de dados de lidos dos sensores.

--- sql_scripts/export_sensorreadevent.sql
+++ sql_scripts/export_sensorreadevent.sql
@@ -1,5 +1,5 @@
 SELECT 
-	DATE_FORMAT(timestamp, "%Y-%m-%d %H:%i:%s") AS 'data/hora',
+	DATE_FORMAT(CONVERT_TZ(timestamp,'+00:00','-03:00'), "%Y-%m-%d %H:%i:%s") AS 'data/hora',
    (SELECT sensor_id FROM model_sensor WHERE id = a.sensor_id) AS 'sensor',
    (SELECT 
 		(SELECT 
@@ -17,5 +17,7 @@ FROM model_sensorreadevent a
 WHERE 
    sensor_id NOT IN (1, 2, 3, 4, 5, 8, 9, 10, 11, 12) AND 
    timestamp > '2019-09-27 20:35:00' AND
-   ((sensor_id IN (7, 15) AND value_read <> 0) OR (sensor_id NOT IN (7, 15)))
+   ((sensor_id IN (7, 15) AND value_read <> 0) OR (sensor_id NOT IN (7, 15))) AND
+   ((sensor_id IN (6, 14) AND value_read <> 0) OR (sensor_id NOT IN (6, 14))) AND
+   ((sensor_id IN (17, 18) AND value_read <> 9) OR (sensor_id NOT IN (17, 18)))
 ORDER BY timestamp;
